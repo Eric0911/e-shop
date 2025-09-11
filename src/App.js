996bb4d0b1d9b34b14aaccbd5a9ed10d72739432
Clark <vbnm4628@gmail.com>
2025-09-11 10:38:56 +0800

    更新程式

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div>
+          <div className='fix-header'>
+            <ul className='left-menu'>
+              <li><a href='/'>回首頁</a></li>
+            </ul>
+            <ul className='right-menu'>
+              <li className='menu-bar'><a href='/'>登入</a></li>
+              <li className='menu-bar'><a href='/'>註冊</a></li>
+              <li className='menu-bar'><a href='/'>會員中心</a></li>
+              <li className='menu-bar'><a href='/'>查訂單</a></li>
+              <li className='menu-bar'><a href='/'>追蹤清單</a></li>
+              <li className='menu-bar'><a href='/'>折價券</a></li>
+              <li className='menu-bar'><a href='/'>購物車</a></li>
+            </ul>
+          </div>
+        </div>
+        <div className='header coding'>
+          <div className='logo'>
+            Logo
+          </div>
+          <div className='search-bar'>
+            <input type="text" placeholder='請輸入關鍵字或品號' />
+            <button>搜尋</button>
+          </div>
+          <div className='sales-promotion'>
+
+          </div>
+        </div>
       </header>
+      <div className='banner coding'>
+      </div>
+      <div className='content-1 coding'>
+      </div>
+      <div className='content-2 coding'>
+      </div>
+      <div className='friend-link coding'>
+      </div>
+      <div className='facebook-area coding'>
+      </div>
+      <div className='footer-1 coding'>
+      </div>
+      <div className='footer-2 coding'>
+      </div>
+      <div className='footer-3 coding'>
+      </div>
     </div>
   );
 }
