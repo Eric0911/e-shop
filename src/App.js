49b69905f2ba6e145106a76c587d90f621002033
Clark <vbnm4628@gmail.com>
2025-09-21 12:58:44 +0800

    更新程式

--- src/App.js
+++ src/App.js
@@ -20,33 +20,71 @@ function App() {
             </ul>
           </div>
         </div>
-        <div className='header coding'>
-          <div className='logo'>
-            Logo
+        <div className='header'>
+          <div className='search-area'>
+            <div className='logo'>
+              LOGO
+            </div>
+            <div className='search-bar'>
+              <input type="text" placeholder='請輸入關鍵字或品號' />
+              <button>搜尋</button>
+            </div>
+            <div className='sales-promotion'>
+              <li>
+                <a href='/'>
+                  <img src="/images/sales-promotion_1.png" alt="/"></img>
+                </a>
+              </li><li>
+                <a href='/'>
+                  <img src="/images/sales-promotion_2.png" alt="/"></img>
+                </a>
+              </li>
+              <li>
+                <a href='/'>
+                  <img src="/images/sales-promotion_3.png" alt="/"></img>
+                </a>
+              </li>
+            </div>
           </div>
-          <div className='search-bar'>
-            <input type="text" placeholder='請輸入關鍵字或品號' />
-            <button>搜尋</button>
-          </div>
-          <div className='sales-promotion'>
-            <li>
-              <a href='/'>
-                <img src="../images/sales-promotion_1.png"></img>
-              </a>
-            </li><li>
-              <a href='/'>
-                <img src="../images/sales-promotion_2.png"></img>
-              </a>
-            </li>
-            <li>
-              <a href='/'>
-                <img src="../images/sales-promotion_3.png"></img>
-              </a>
-            </li>
-          </div>
+          <div className='tab-area'>
+            <div className='menu-tab'>
+              <li>Plus訂閱</li>
+              <li>站長推薦</li>
+              <li>重磅新品</li>
+              <li>樂公益</li>
+              <li>保險館</li>
+              <li>看看買</li>
+              <li>週期訂購</li>
+              <li>品牌旗艦</li>
+              <li>綠色生活</li>
+              <li>賺代幣</li>
+            </div>  
+          
+          </div> 
         </div>
       </header>
       <div className='banner coding'>
+        <div className='banner-left'>
+          <div className='menu-title'>
+            <img src="/images/menu-title.png" alt="/"></img>
+          </div> 
+        </div>
+        <div className='banner-middle'>
+          <img src="/images/banner_0.jpg" alt="/"></img>
+        </div> 
+        <div className='banner-right coding'>
+          <div className='banner-right-title'>
+          </div>
+          <div className='banner-right-promo'>
+          
+          </div>
+          <div className='banner-right-promo'>
+          
+          </div>
+          <div className='banner-right-promo'>
+          
+          </div>
+        </div>
       </div>
       <div className='content-1 coding'>
       </div>
